30e9fddafd340eefc48a2b0796434cecbd774567
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-04-20 05:03:02 +0100

    created and set up notes schema

---
+++ lesson_1/backend/model/Note.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import sequence from "mongoose-sequence";
+const AutoIncrement = sequence(mongoose);
+
+const noteSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true },
+);
+
+noteSchema.plugin(AutaoIncrement, {
+  inc_field: "ticket",
+  id: "ticketNums",
+  starts_seq: 500,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+export default Note;
