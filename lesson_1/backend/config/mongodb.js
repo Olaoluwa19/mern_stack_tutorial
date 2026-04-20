a87aa0d3f77142e5849b59c857175b2092adf08b
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-04-20 04:59:55 +0100

    renamed db to CompanyDB

--- lesson_1/backend/config/mongodb.js
+++ lesson_1/backend/config/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGO_URI}/techNotes`);
+    await mongoose.connect(`${process.env.MONGO_URI}/CompanyDB`);
   } catch (err) {
     console.error(err);
   }
