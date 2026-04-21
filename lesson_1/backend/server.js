1bc97c6ad78e919c3c04ab6de4bef5e7b31211d2
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-04-21 05:14:29 +0100

    feat: modified mongoDb connection in server.js

--- lesson_1/backend/server.js
+++ lesson_1/backend/server.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import http from "http";
 import app from "./app.js";
 import connectDB from "./config/mongodb.js";
+import { logEvents } from "./middleware/logEvents.js";
 
 const PORT = process.env.PORT || 3500;
 
@@ -10,6 +11,12 @@ const server = http.createServer(app);
 
 const startServer = async () => {
   try {
+    // check mongoDB error first before connecting to the server
+    mongoose.connection.on("error", (err) => {
+      console.error("MongoDB connection error:", err);
+      logEvents(`${err.code}\t${err.message}`, "mongoErrLog.log");
+    });
+
     // connect to mongoDB
     connectDB();
     mongoose.connection.once("open", () => {
@@ -18,8 +25,13 @@ const startServer = async () => {
         console.log(`Server running on port ${PORT} 🚀🚀`);
       });
     });
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
+  } catch (err) {
+    await logEvents(
+      `${err.code || err.no || "UNKNOWN"}\t${err.syscall || ""}\t${err.hostname || ""}`,
+      "mongoErrLog.log",
+    );
+    console.error(`MongoDB connection error: ${err.message}`);
+    process.exit(1);
   }
 };
 
