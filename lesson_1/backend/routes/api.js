7e396b1d27ddebd31166d5aa39f10c70d26da779
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-04-21 05:17:04 +0100

    feat: mounted userRoutes in api

--- lesson_1/backend/routes/api.js
+++ lesson_1/backend/routes/api.js
@@ -1,9 +1,11 @@
 import express from "express";
 import root from "./root.js";
+import userRoutes from "./api/userRoutes.js";
 
 const api = express.Router();
 
 // Define API routes here
 api.use("/", root);
+api.use("/users", userRoutes);
 
 export default api;
