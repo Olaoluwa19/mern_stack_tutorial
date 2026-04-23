dc670a0efb6be61868ef38b407517baf2df94c9e
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-04-23 04:51:04 +0100

    feat: completed CRUD operation for User controller

--- lesson_1/backend/controllers/usersController.js
+++ lesson_1/backend/controllers/usersController.js
@@ -1,6 +1,15 @@
 import asyncHandler from "express-async-handler";
 import UserService from "../services/userService.js";
-import { noContent, ok } from "../utility/response.js";
+import {
+  ok,
+  noContent,
+  created,
+  badRequest,
+  notFound,
+  conflict,
+} from "../utility/response.js";
+import { hashPassword } from "../utility/password.js";
+import NoteService from "../services/noteService.js";
 
 // @desc  Get all users
 // @route GET /api/users
@@ -15,28 +24,107 @@ const getAllUsers = asyncHandler(async (req, res) => {
 // @route GET /users:id
 // @access Private/Admin
 const getUser = asyncHandler(async (req, res) => {
-  res.send("Get user");
+  const { id } = req.params;
+  const user = await UserService.findUserById(id);
+  if (!user) return noContent(res);
+  return ok(res, user);
 });
 
 // @desc  Create new user
 // @route POST /users
 // @access Private/Admin
 const createNewUser = asyncHandler(async (req, res) => {
-  res.send("Create user");
+  const { username, password, roles } = req.body;
+
+  // Confirm Data
+  if (!username || !password || !Array.isArray(roles) || !roles.length) {
+    return badRequest(res, "All fields are required");
+  }
+  // Validate role
+  if (!UserService.validateRole(roles)) {
+    return badRequest(`Invalid role specified, ${roles} is not allowed.`);
+  }
+
+  // Check for duplicate username
+  const duplicate = await UserService.checkDuplicateUser(username);
+  if (duplicate) return conflict(res, "Username already exists");
+
+  // validate password
+  if (!validatePassword(password)) {
+    return badRequest(
+      res,
+      "Password must be at least 6 characters and include uppercase, lowercase, number, and special character.",
+    );
+  }
+
+  // Hash password
+  const hashedPwd = await hashPassword(hashPassword);
+  const userObject = { username, password: hashedPwd, roles };
+
+  // Create and store new user
+  const user = await UserService.createUser(userObject);
+
+  if (user) {
+    return created(res, user, `New user ${user.username} created`);
+  } else {
+    return badRequest(res, "Invalid user data received");
+  }
 });
 
 // @desc  Update a user
 // @route PATCH /users
 // @access Private/Admin
 const updateUser = asyncHandler(async (req, res) => {
-  res.send("Update user");
+  const { id, username, roles, active, password } = req.body;
+
+  // Confirm data
+  if (
+    !id ||
+    !username ||
+    !Array.isArray(roles) ||
+    !roles.length ||
+    typeof active !== "boolean"
+  ) {
+    return badRequest(
+      res,
+      "All fields except password are required and must be valid",
+    );
+  }
+
+  const user = await UserService.findUserById(id);
+  if (!user) return notFound(res, "User not found");
+
+  // Check for duplicate
+  const duplicate = await UserService.checkDuplicateUser(username);
+  // Allow update to the original user, but prevent changing to another existing username
+  if (duplicate && duplicate?._id.toString() !== id) {
+    return conflict(res, "Username already exists");
+  }
+  const hashPwd = password ? await hashPassword(password) : null;
+
+  const updatedUser = await UserService.updateUser(user, hashPwd);
+
+  return ok(res, updateUser, `User ${updatedUser.username} updated`);
 });
 
 // @desc  Delete user
 // @route DELETE /users
 // @access Private/Admin
 const deleteUser = asyncHandler(async (req, res) => {
-  res.send("Delete user");
+  const { id } = req.body;
+
+  if (!id) return badRequest(res, "User ID required");
+
+  const notes = await NoteService.findUserNotes(id);
+  if (notes?.length) {
+    return badRequest(res, "User has assigned notes");
+  }
+
+  const user = await UserService.findUserById(id);
+  if (!user) return notFound(res, "User not found");
+
+  const result = await UserService.deleteUser(id);
+  return ok(res, result, `User ${user.username} with ID ${user._id} deleted`);
 });
 
 export { getAllUsers, getUser, createNewUser, updateUser, deleteUser };
