e1a4f79b3363df7c39542ddf875d2ed36d615a2d
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-04-23 03:15:01 +0100

    feat: added getAllUsers logic

--- lesson_1/backend/controllers/usersController.js
+++ lesson_1/backend/controllers/usersController.js
@@ -1,37 +1,41 @@
 import asyncHandler from "express-async-handler";
+import UserService from "../services/userService.js";
+import { noContent, ok } from "../utility/response.js";
 
 // @desc  Get all users
 // @route GET /api/users
 // @access Private/Admin
-const getAllUsers = asyncHandler((req, res) => {
-  res.send("Get all users");
+const getAllUsers = asyncHandler(async (req, res) => {
+  const users = await UserService.findAllUsers();
+  if (!users?.length) return noContent(res);
+  return ok(res, users);
 });
 
 // @desc  Get a user
 // @route GET /users:id
 // @access Private/Admin
-const getUser = asyncHandler((req, res) => {
+const getUser = asyncHandler(async (req, res) => {
   res.send("Get user");
 });
 
 // @desc  Create new user
 // @route POST /users
 // @access Private/Admin
-const createNewUser = asyncHandler((req, res) => {
+const createNewUser = asyncHandler(async (req, res) => {
   res.send("Create user");
 });
 
 // @desc  Update a user
 // @route PATCH /users
 // @access Private/Admin
-const updateUser = asyncHandler((req, res) => {
+const updateUser = asyncHandler(async (req, res) => {
   res.send("Update user");
 });
 
 // @desc  Delete user
 // @route DELETE /users
 // @access Private/Admin
-const deleteUser = asyncHandler((req, res) => {
+const deleteUser = asyncHandler(async (req, res) => {
   res.send("Delete user");
 });
 
