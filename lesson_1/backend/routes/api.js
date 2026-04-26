3034e78015c63884059d47b8ae3801dff969b083
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-04-26 03:47:23 +0100

    feat: mounted noteRoutes to api route

--- lesson_1/backend/routes/api.js
+++ lesson_1/backend/routes/api.js
@@ -1,11 +1,13 @@
 import express from "express";
 import root from "./root.js";
 import userRoutes from "./api/userRoutes.js";
+import noteRoutes from "./api/noteRoutes.js";
 
 const api = express.Router();
 
 // Define API routes here
 api.use("/", root);
 api.use("/users", userRoutes);
+api.use("/notes", noteRoutes);
 
 export default api;
