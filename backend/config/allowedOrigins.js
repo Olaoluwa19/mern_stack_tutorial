314e8611589508f4b9d562f7cd8835f931409030
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-04-29 04:06:43 +0100

    feat: added frontend url to cors allowedOrigins

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   "https://www.dandrepairshop.com",
   "http://dandrepairshop.com",
-  "http://localhost:3000",
+  "http://localhost:5173",
 ];
 
 export default allowedOrigins;
