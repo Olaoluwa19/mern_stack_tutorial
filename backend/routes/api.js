b45847bc8d2e374d435d770cf66b67c468871e10
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-05-15 04:10:30 +0100

    feat: mounted authRouter and made changes to naming in the other routers

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,13 +1,15 @@
 import express from "express";
 import root from "./root.js";
-import userRoutes from "./api/userRoutes.js";
-import noteRoutes from "./api/noteRoutes.js";
+import userRouter from "./api/userRoutes";
+import noteRouter from "./api/noteRoutes";
+import authRouter from "./api/authRoutes.js";
 
 const api = express.Router();
 
 // Define API routes here
 api.use("/", root);
-api.use("/users", userRoutes);
-api.use("/notes", noteRoutes);
+api.use("/users", userRouter);
+api.use("/notes", noteRouter);
+api.use("/auth", authRouter);
 
 export default api;
