c283cbee04cbd7b4585027b26c555a4918c41ba2
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-06-01 05:14:16 +0100

    feat: refactored Prefetch

--- frontend/src/features/auth/Prefetch.jsx
+++ frontend/src/features/auth/Prefetch.jsx
@@ -1,28 +1,19 @@
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 
 import { store } from "../../app/store";
 import { notesApiSlice } from "../notes/notesApiSlice";
 import { usersApiSlice } from "../users/usersApiSlice";
-import { selectCurrentToken } from "./authSlice";
 
 const Prefetch = () => {
-  const token = useSelector(selectCurrentToken);
-
   useEffect(() => {
-    if (!token) return; // ← Important: Don't run without token
-
-    console.log("✅ Token found → Subscribing to notes & users");
-    const notes = store.dispatch(notesApiSlice.endpoints.getNotes.initiate());
-    const users = store.dispatch(usersApiSlice.endpoints.getUsers.initiate());
-
-    return () => {
-      console.log("unsubscribing");
-      notes.unsubscribe();
-      users.unsubscribe();
-    };
-  }, [token]);
+    store.dispatch(
+      notesApiSlice.util.prefetch("getNotes", "notesList", { force: true }),
+    );
+    store.dispatch(
+      usersApiSlice.util.prefetch("getUsers", "usersList", { force: true }),
+    );
+  }, []);
 
   return <Outlet />;
 };
