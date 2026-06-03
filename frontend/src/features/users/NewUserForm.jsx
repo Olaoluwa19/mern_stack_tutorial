3737f29f5d0e1fe2c14c81125a3d963512071b03
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-06-03 03:30:13 +0100

    feat: added useTitle hook

--- frontend/src/features/users/NewUserForm.jsx
+++ frontend/src/features/users/NewUserForm.jsx
@@ -4,11 +4,13 @@ import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSave } from "@fortawesome/free-solid-svg-icons";
 import { ROLES } from "../../config/ROLES";
+import useTitle from "../../hooks/useTitle";
 
 const USER_REGEX = /^[A-z]{3,20}$/;
 const PWD_REGEX = /^[A-z0-9!@#$%]{4,12}$/;
 
 const NewUserForm = () => {
+  useTitle("TechNotes: New User");
   const [addNewUser, { isLoading, isSuccess, isError, error }] =
     useAddNewUserMutation();
 
