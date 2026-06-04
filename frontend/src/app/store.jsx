dc86f9528981cd617174e0e56cfcded182189351
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-06-04 02:14:39 +0100

    set devTools to false for production

--- frontend/src/app/store.jsx
+++ frontend/src/app/store.jsx
@@ -10,7 +10,7 @@ export const store = configureStore({
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
-  devTools: true,
+  devTools: false, // true for development, false for production
 });
 
 setupListeners(store.dispatch);
