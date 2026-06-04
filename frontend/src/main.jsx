0a9fa4731767f00bee3d47c5fd15d92d13a5da52
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-06-04 02:14:15 +0100

    added disable react dev tools

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import { store } from "./app/store.jsx";
 import { Provider } from "react-redux";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (import.meta.env.PROD) {
+  disableReactDevTools();
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
